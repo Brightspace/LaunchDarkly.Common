d83abeec35cfb35152672bc84c940b7db9323085
Jeff Ashton <Jeff.Ashton@D2L.com>
2019-05-06 14:32:28 -0400

    Removing duplicate attribute

--- src/LaunchDarkly.Common/AssemblyInfo.cs
+++ src/LaunchDarkly.Common/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("LaunchDarkly.Client")]
-[assembly: InternalsVisibleTo("LaunchDarkly.Tests")]
-
 [assembly: InternalsVisibleTo("LaunchDarkly.Common.Tests")]
 [assembly: InternalsVisibleTo("LaunchDarkly.Xamarin")]
 [assembly: InternalsVisibleTo("LaunchDarkly.Xamarin.Tests")]
